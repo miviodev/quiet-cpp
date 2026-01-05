e329f89ee0f3415e06802996abf015b229bafad1
miviodev <miviodev@gmail.com>
2026-01-05 20:38:34 +0300

    add base logic

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
-int main() {
-    std::cout << "hello world!";
+#include <string>
+#include <vector>
+using namespace std;
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        std::vector<std::string> all_args;
+        string args;
+        if (argc > 1) {
+            all_args.assign(argv + 1, argv + argc);
+        }
+        for (auto str : all_args) {
+            args=args+" "+ str;
+        }
+        args = args + " > log.txt";
+        //args = args + " >> log.txt>&1"; // FOR WINDOWS
+        int command = system(args.c_str());
+        if (command == 0) {
+            cout << "Success!" << endl;
+        } else {
+            cout << command << endl;
+        }
+    } else {
+        cout << "[ERROR] Enter command" << endl;
+    }
     return 0;
 }
